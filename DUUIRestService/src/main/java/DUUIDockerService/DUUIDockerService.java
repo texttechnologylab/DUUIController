c21f807ce064465b8e23b17deb961446fe10e1b3
CedricBorko <cedric@borkoland.de>
2023-08-21 15:30:13 +0200

    Example DUUI Usage and note for Windows support

--- DUUIRestService/src/main/java/DUUIDockerService/DUUIDockerService.java
+++ DUUIRestService/src/main/java/DUUIDockerService/DUUIDockerService.java
@@ -1,7 +1,12 @@
 package DUUIDockerService;
 
+import DUUIReponse.DUUIStandardResponse;
+import DUUIReponse.DUUIStatusResponse;
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import java.io.IOException;
+import java.net.URISyntaxException;
 import org.apache.uima.UIMAException;
 import org.apache.uima.fit.factory.JCasFactory;
 import org.apache.uima.fit.util.JCasUtil;
@@ -14,53 +19,52 @@ import org.xml.sax.SAXException;
 import spark.Request;
 import spark.Response;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
-
 public class DUUIDockerService {
 
-    private DUUIComposer composer;
-    private DUUIDockerDriver driver;
-    private String _processIdentifier;
-    private final JCas document;
+  private DUUIComposer composer;
+  private DUUIDockerDriver driver;
+  private String _processIdentifier;
+  private final JCas document;
 
-    public DUUIDockerService() throws UIMAException, IOException, SAXException {
-        document = JCasFactory.createJCas();
+  public DUUIDockerService() throws UIMAException, IOException, SAXException {
+    document = JCasFactory.createJCas();
+  }
+
+  public static Object build(Request request, Response response)
+    throws IOException, URISyntaxException, UIMAException, SAXException {
+
+    try {
+      ObjectMapper mapper = new ObjectMapper();
+      DUUIDockerComponentPayload componentPayload = mapper.readValue(
+        request.body(),
+        DUUIDockerComponentPayload.class
+      );
+      System.out.println(componentPayload.getTimeout());
+      System.out.println(componentPayload.getImageURL());
+      System.out.println(componentPayload.getScale());
+      response.status(200);
+      return new Gson()
+        .toJsonTree(new Gson().toJsonTree(componentPayload)
+        );
+    } catch (JsonParseException e) {
+      response.status(400);
     }
 
-    public static Object build(Request request, Response response) throws IOException, URISyntaxException, UIMAException, SAXException {
-        DUUIDockerService service = new DUUIDockerService();
+    return "Error";
+  }
 
-        try {
-            ObjectMapper mapper = new ObjectMapper();
-            DUUIDockerComponentPayload componentPayload = mapper.readValue(request.body(), DUUIDockerComponentPayload.class);
-            System.out.println(componentPayload.getTimeout());
-            System.out.println(componentPayload.getImageURL());
-            response.status(200);
+  public DUUIDockerService withTimeout(int timeoutMilliseconds) {
+    System.out.println(timeoutMilliseconds);
+    //        driver.withTimeout(timeoutMilliseconds);
+    return this;
+  }
 
-        } catch (JsonParseException e) {
-            response.status(400);
-        }
+  public void startComposer(String processIdentifier) throws Exception {
+    _processIdentifier = processIdentifier;
+    composer.run(document, processIdentifier);
+  }
 
-
-        return "Docker";
-    }
-
-    public DUUIDockerService withTimeout(int timeoutMilliseconds) {
-        System.out.println(timeoutMilliseconds);
-//        driver.withTimeout(timeoutMilliseconds);
-        return this;
-    }
-
-
-    public void startComposer(String processIdentifier) throws Exception {
-        _processIdentifier = processIdentifier;
-        composer.run(document, processIdentifier);
-    }
-
-    public String getIdentifier() {
-        return _processIdentifier;
-    }
-
+  public String getIdentifier() {
+    return _processIdentifier;
+  }
 }
-
