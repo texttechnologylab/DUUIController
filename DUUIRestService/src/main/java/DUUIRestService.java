71d5d695cc6636d6870c383a28deab919da31b11
CedricBorko <cedric@borkoland.de>
2023-08-23 16:45:16 +0200

    Build Endpoint

--- DUUIRestService/src/main/java/DUUIRestService.java
+++ DUUIRestService/src/main/java/DUUIRestService.java
@@ -1,7 +1,10 @@
+
 import DUUIDockerService.DUUIDockerService;
 import DUUIReponse.DUUIStandardResponse;
 import DUUIReponse.DUUIStatusResponse;
 import com.google.gson.Gson;
+import spark.Request;
+import spark.Response;
 
 import java.util.Random;
 import java.util.HashMap;
@@ -10,26 +13,18 @@ import static spark.Spark.*;
 
 public class DUUIRestService {
 
-
+    private static Object build(Request request, Response response) {
+        System.out.println(request.body());
+        return "Test";
+    }
     public static void main(String[] args) {
-        final HashMap<Integer, String> statusMap = new HashMap<>();
-        Random random = new Random();
-        statusMap.put(1, "Waiting");
-        statusMap.put(2, "Done");
-        statusMap.put(3, "In Progress");
-        statusMap.put(4, "Canceled");
 
         port(9090);
 
         get("/", (request, response) -> "DUUI says hello");
-        get("/docker", "application/json", DUUIDockerService::build);
-        get("/status/:id", (request, response) -> {
-            response.type("application/text");
-            int index = random.nextInt(0, 4);
-            return new Gson().toJson(new DUUIStandardResponse(
-                    new Gson().toJsonTree(new DUUIStatusResponse(statusMap.get(index)))
-            ));
-        });
+
+        post("/build", "application/json", DUUIRestService::build);
+
     }
 
 }
